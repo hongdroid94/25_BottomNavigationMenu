4eab87cd0db33c9e2dfa6062ae904ad7e573a426
이홍철 <hongdroid94@gmail.com>
2019-10-07 03:18:22 +0900

    first commit

---
+++ app/src/main/java/com/example/bottomnavi/Frag1.java
@@ -0,0 +1,24 @@
+package com.example.bottomnavi;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag1 extends Fragment {
+
+    private View view;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag1, container, false);
+
+
+        return view;
+    }
+}
